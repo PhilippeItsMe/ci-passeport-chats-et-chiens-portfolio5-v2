52ca828a5e6eda47295fa3cc002465ccbfad7d63
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-02-26 10:42:56 +0000

    Intagrate grpd links in the footer

--- grpd/views.py
+++ grpd/views.py
@@ -1,10 +1,11 @@
 from django.views.generic import TemplateView
 
 class LegalNoticeView(TemplateView):
-    template_name = "legal_notice.html"
+    template_name = "grpd/legal_notice.html"
 
 class CookiesNoticeView(TemplateView):
-    template_name = "cookies_notice.html"
+    template_name = "grpd/cookies_notice.html"
 
 class PrivacyPolicyView(TemplateView):
-    template_name = "privacy_policy.html"
+    template_name = "grpd/privacy_policy.html"
+    
