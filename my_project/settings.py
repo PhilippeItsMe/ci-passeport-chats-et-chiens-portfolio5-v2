1c2c3ffc5ee06278f16698f9dae134dd56fd4c88
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-03-05 16:05:49 +0000

    Still debugging vouchers pdf issue

--- my_project/settings.py
+++ my_project/settings.py
@@ -156,5 +156,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
