24716f90e816b1b7f2361f8f4181222a7100212b
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-04-03 09:33:09 +0000

    Add pet page

--- pet_owners/urls.py
+++ pet_owners/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('pet_owner_form/', views.pet_owner_form, name='pet_owner_form'),
-    path('pets/add/', views.pet_create, name='pet-create'),      
-    path('pets/<int:pet_id>/edit/', views.pet_edit, name='pet-edit'),
-    path('pets/<int:pet_id>/delete/', views.pet_delete, name='pet-delete'),
+    path('pets/', views.pet_list, name='pet_list'),    
+    path('pets/add/', views.pet_create, name='pet_create'),      
+    path('pets/<int:pet_id>/edit/', views.pet_edit, name='pet_edit'),
+    path('pets/<int:pet_id>/delete/', views.pet_delete, name='pet_delete'),
 ]
--- pet_owners/views.py
+++ pet_owners/views.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-#from django.urls import reverse
 from django.contrib import messages
 from .models import PetOwner, Pet
 from .forms import PetOwnerForm, PetForm
 from pet_businesses.utils import group_required
-#from django.core.exceptions import PermissionDenied
 
 
 @group_required("Pet Owners")
@@ -35,10 +33,24 @@ def pet_owner_form(request):
                   {'form': form, 'pet_owner': pet_owner})
 
 
-@login_required
+@group_required("Pet Owners")
+def pet_list(request):
+    """
+    View to list all pets of the logged-in PetOwner
+    """
+    pet_owner = get_object_or_404(PetOwner, author=request.user)
+    pets = Pet.objects.filter(pet_owner=pet_owner)
+
+    return render(request, 'pet_owners/pet_owners_pets.html', {
+        'pets': pets,
+        'pet_owner': pet_owner
+    })
+
+
+@group_required("Pet Owners")
 def pet_create(request):
     """
-    View to create a pet
+    View to create a new pet
     """
     pet_owner = get_object_or_404(PetOwner, author=request.user)
 
@@ -49,13 +61,17 @@ def pet_create(request):
             pet.pet_owner = pet_owner
             pet.save()
             messages.success(request, f"L'animal {pet.name} a été ajouté avec succès.")
-            return redirect('pet_owner_form')
+            return redirect('pet_list')
         else:
             messages.error(request, "Il y a eu une erreur.")
     else:
         form = PetForm()
 
-    return render(request, 'pet_owners/pet_owners_form.html', {'form': form, 'pet_owner': pet_owner})
+    return render(request, 'pet_owners/pet_form.html', {
+        'form': form,
+        'pet_owner': pet_owner,
+    })
+
 
 
 @group_required("Pet Owners")
@@ -70,13 +86,18 @@ def pet_edit(request, pet_id):
         if form.is_valid():
             pet = form.save()
             messages.success(request, f"L'animal {pet.name} a été mis à jour avec succès.")
-            return redirect('pet_owner_form')
+            return redirect('pet_list')
         else:
             messages.error(request, "Il y a eu une erreur.")
     else:
         form = PetForm(instance=pet)
 
-    return render(request, 'pet_owners/pet_owners_form.html', {'form': form, 'pet': pet, 'pet_owner': pet.pet_owner})
+    return render(request, 'pet_owners/pet_form.html', {
+        'form': form,
+        'pet': pet,
+        'pet_owner': pet.pet_owner,
+    })
+
 
 
 @group_required("Pet Owners")
@@ -89,8 +110,8 @@ def pet_delete(request, pet_id):
     if request.method == "POST":
         pet.delete()
         messages.success(request, f"L'animal {pet.name} a été supprimé avec succès.")
-        return redirect('pet_owner_form')
+        return redirect('pet_list')
     else:
         messages.error(request, "Il y a eu une erreur.")
 
-    return render(request, 'pet_owners/pet_owners_form.html', {'pet': pet, 'pet_owner': pet.pet_owner})
+    return redirect('pet_list')
