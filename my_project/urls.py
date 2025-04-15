bcd6b81c65802a33112a2a375f790ff57ae49d02
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-04-15 14:00:05 +0000

    Feat : email recovery key templates creation

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
+
+
+  # Email reset
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
     
     # BAG
     path('bag/', include('bag.urls')),
@@ -25,7 +38,7 @@ urlpatterns = [
     path('grpd/', include('grpd.urls')),
 
     # Products
-    path('products/', include('products.urls')), 
+    path('products/', include('products.urls')),   
 ]
 
 handler404 = 'my_project.views.handler404'
