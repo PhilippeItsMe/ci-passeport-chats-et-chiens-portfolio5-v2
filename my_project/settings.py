e896ef8b5c6ebadde4cbbd793056ea3cebf5d775
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-04-16 07:30:25 +0000

    Fix : resetting of the gmail smtp

--- my_project/settings.py
+++ my_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 #------------ Security check -------------- #
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                 '127.0.0.1',
@@ -186,7 +186,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp-relay.brevo.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
