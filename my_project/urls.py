d5995fe656be755a51cf368f430cd65e87840029
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-04-16 10:01:02 +0000

    Fix - debuging wrong email usage for password reset

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,44 +1,46 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from my_project.views import TextOnlyPasswordResetView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
 
-
-  # Email reset
-    path('password_reset/', auth_views.PasswordResetView.as_view(),
-         name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
+     # Email reset
+     path('password_reset/', TextOnlyPasswordResetView.as_view(),
+          name='password_reset'),
+     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+          name='password_reset_done'),
+     path('reset/<uidb64>/<token>/',
+          auth_views.PasswordResetConfirmView.as_view(),
+          name='password_reset_confirm'),
+     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+          name='password_reset_complete'),
+     
+     # Account
+     path('accounts/', include('allauth.urls')),
     
-    # BAG
-    path('bag/', include('bag.urls')),
+     # BAG
+     path('bag/', include('bag.urls')),
 
-    # Checkout
-    path('checkout/', include('checkout.urls')),
+     # Checkout
+     path('checkout/', include('checkout.urls')),
 
-    # Pet Owners
-    path('', include('pet_owners.urls')),
+     # Pet Owners
+     path('', include('pet_owners.urls')),
 
-    # Pet Businesses
-    path('', include('pet_businesses.urls')),
+     # Pet Businesses
+     path('', include('pet_businesses.urls')),
 
-    # Vouchers
-    path('vouchers/', include('vouchers.urls')),
+     # Vouchers
+     path('vouchers/', include('vouchers.urls')),
 
-    # GRPD
-    path('grpd/', include('grpd.urls')),
+     # GRPD
+     path('grpd/', include('grpd.urls')),
 
-    # Products
-    path('products/', include('products.urls')),   
+     # Products
+     path('products/', include('products.urls')),   
 ]
 
 handler404 = 'my_project.views.handler404'
--- my_project/views.py
+++ my_project/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.views import PasswordResetView
 
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
+
+class TextOnlyPasswordResetView(PasswordResetView):
+    email_template_name = 'registration/password_reset_email.txt'
+    subject_template_name = 'registration/password_reset_subject.txt'
+
+    def form_valid(self, form):
+        print(">>> MA VUE EST UTILISÉE <<<")
+        return super().form_valid(form)
