8038ab3a53a2c34346cfe18313493f446a2627a3
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-04-24 09:47:54 +0000

    Fix - pyhton code flak8

--- grpd/models.py
+++ grpd/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class CookieConsent(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) #If logged
-    session_key = models.CharField(max_length=255, null=True, blank=True) #If not logged
-    accepted_essential = models.BooleanField(default=True)  
+    """
+    Model do manage grpd consent
+    """
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)  # If logged
+    session_key = models.CharField(
+        max_length=255, null=True, blank=True)  # If not logged
+    accepted_essential = models.BooleanField(default=True)
     accepted_analytics = models.BooleanField(default=False)
     accepted_marketing = models.BooleanField(default=False)
     date_consent_given = models.DateTimeField(auto_now_add=True)
--- grpd/urls.py
+++ grpd/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PrivacyPolicyView,CGV, save_cookie_consent
+from .views import PrivacyPolicyView, CGV, save_cookie_consent
 from .views import LegalNoticeView, CookiesNoticeView
 
 urlpatterns = [
     path('conditions-generales-de-vente/', CGV.as_view(), name='cgv'),
     path('mentions-legales/', LegalNoticeView.as_view(), name='legal_notice'),
-    path('notice-cookies/', CookiesNoticeView.as_view(), name='cookies_notice'),
+    path('notice-cookies/', CookiesNoticeView.as_view(),
+         name='cookies_notice'),
     path('politique-confidentialite/',
          PrivacyPolicyView.as_view(), name='privacy_policy'),
     path("save-cookie-consent/",
